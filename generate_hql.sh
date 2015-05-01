d30a88161ac1a69ff3cd565cf6d108e5111d1133
dsalmela@palominodb.com <dsalmela@palominodb.com>
2015-05-01 17:14:40 -0500

    changed db

--- generate_hql.sh
+++ generate_hql.sh
@@ -4,9 +4,9 @@ TABLES="user
 for t in $TABLES
 do
 
-   columns=$(hive -S -e "use mytest;show columns in $t"| tr -d ' '|paste -sd,|sed -r 's/(latitude|longitude)/0.0 AS &/g')  
+   columns=$(hive -S -e "use testdb;show columns in $t"| tr -d ' '|paste -sd,|sed -r 's/(latitude|longitude)/0.0 AS &/g')  
     echo --------------------------------$t---------------------------------
-    echo "INSERT OVERWRITE TABLE $t"
+    echo "use testdb;INSERT OVERWRITE TABLE $t"
     echo "SELECT $columns"
     echo "FROM $t ;"
 done
