4f9dd9b5a88e8997471476acd739db7947709ce1
dsalmela@palominodb.com <dsalmela@palominodb.com>
2015-05-01 16:54:25 -0500

    no message

--- run_sample.sh
+++ run_sample.sh
@@ -1,8 +1,15 @@
+
+hadoop fs -mkdir mytest
+hadoop fs -put users.csv mytest/
+
 hive -e "
+
 create database if not exists mytest;
 use mytest;
 
-create table if not exists user (
+drop table if exists user;
+
+create external table if not exists user (
   user_id int, 
   email string, 
   fname string, 
@@ -17,6 +24,10 @@ create table if not exists user (
   datestamp string, 
   hour string  
   )
+ row format delimited fields terminated by ',' stored as textfile
 ;
+
+
 load data inpath '/user/cloudera/mytest/users.csv' overwrite into TABLE user;
 "
+
